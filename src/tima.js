1d134a974c1e48e3bea816e0df3b8214d8c025f5
imkimchi <wbvcos@gmail.com>
2018-09-11 14:11:03 +0900

    better usage

--- src/tima.js
+++ src/tima.js
@@ -1,13 +1,20 @@
-function tima (endDate) {
-  let days, hours, minutes, seconds
+function tima (container, endDate) {
   const display = num => num != 0 ? num : ''
+  let $container = document.querySelector(container)
+  if(!$container) throw new Error(`Container doesn't exists.`)
+  let days, hours, minutes, seconds
+
   endDate = new Date(endDate).getTime()
-  
-  if (isNaN(endDate)) return
-  
-  setInterval(calculate, 1000)
-  
+
+  if (isNaN(endDate)) throw new Error('Invalid end date.')
+
+  calculate()
+  let timer = setInterval(calculate, 1000)
+
   function calculate() {
+    const basicElem = document.getElementById('seconds')
+    !basicElem && init($container)
+
     let startDate = new Date()
     startDate = startDate.getTime()
     let timeRemaining = parseInt((endDate - startDate) / 1000)
@@ -15,21 +22,28 @@ function tima (endDate) {
     if (timeRemaining >= 0) {
       days = parseInt(timeRemaining / 86400)
       timeRemaining = (timeRemaining % 86400)
-      
       hours = parseInt(timeRemaining / 3600)
       timeRemaining = (timeRemaining % 3600)
-      
       minutes = parseInt(timeRemaining / 60)
       timeRemaining = (timeRemaining % 60)
-      
       seconds = parseInt(timeRemaining)
-      
+
       document.getElementById("days").innerHTML = display(days)
       document.getElementById("hours").innerHTML = display(hours)
       document.getElementById("minutes").innerHTML = display(minutes)
       document.getElementById("seconds").innerHTML = display(seconds)
+
     } else {
-      return
+      $container.parentNode.removeChild($container)
+      clearInterval(timer)
     }
+  }
+
+  function init (container) {
+    ['days', 'hours', 'minutes', 'seconds'].forEach(time => {
+      let el = document.createElement('h1')
+      el.setAttribute('id', time)
+      container.appendChild(el)
+    })
   }
 }
